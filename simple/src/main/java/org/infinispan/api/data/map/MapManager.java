ce46c2ca785429ac35aa80de862ae864b3d01ca9
Katia Aresti <karesti@redhat.com>
2018-10-02 18:04:38 +0200

    polish

--- simple/src/main/java/org/infinispan/api/data/map/MapManager.java
+++ simple/src/main/java/org/infinispan/api/data/map/MapManager.java
@@ -3,4 +3,6 @@ package org.infinispan.api.data.map;
 public interface MapManager {
 
    <K, V> InfinispanMap<K, V> getMap(String name);
+
+   //<K, V> InfinispanAsyncMap<K, V> getAsynMap(String name);
 }
