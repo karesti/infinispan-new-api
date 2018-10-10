4f1222b1750fd60d389ad82f0c83fc4a310d57b4
Katia Aresti <karesti@redhat.com>
2018-10-10 13:24:36 +0200

    Map config

---
+++ simple-api/src/main/java/org/infinispan/api/collection/map/MapConfig.java
@@ -0,0 +1,8 @@
+package org.infinispan.api.collection.map;
+
+public class MapConfig {
+
+   public static MapConfig getDefault() {
+      return new MapConfig();
+   }
+}
--- simple/src/main/java/org/infinispan/api/collection/impl/embedded/map/impl/embedded/EmbeddedMapManager.java
+++ simple/src/main/java/org/infinispan/api/collection/impl/embedded/map/impl/embedded/EmbeddedMapManager.java
@@ -3,18 +3,29 @@ package org.infinispan.api.collection.impl.embedded.map.impl.embedded;
 
 import org.infinispan.api.collection.map.InfinispanAsyncMap;
 import org.infinispan.api.collection.map.InfinispanMap;
+import org.infinispan.api.collection.map.MapConfig;
 import org.infinispan.api.collection.map.MapManager;
 
 public class EmbeddedMapManager implements MapManager {
 
    @Override
-   public boolean defineMap(String name, Object configuration) {
-      return true;
+   public boolean defineMapConfig(String configName, MapConfig configuration) {
+      return false;
    }
 
    @Override
    public <K, V> InfinispanMap<K, V> getOrCreateMap(String name) {
       return new EmbeddedCache<>();
+   }
+
+   @Override
+   public <K, V> InfinispanMap<K, V> getOrCreateMap(String name, String configName) {
+      return null;
+   }
+
+   @Override
+   public <K, V> InfinispanMap<K, V> getOrCreateMap(String name, MapConfig config) {
+      return null;
    }
 
    @Override
